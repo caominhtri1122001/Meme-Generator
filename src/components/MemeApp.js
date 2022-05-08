cc90ec919ede50a8dda73e400cdd74126e974e33
Cao Minh Tri <caominhtri112201@gmail.com>
2022-05-08 11:14:01 +0700

    class component

--- src/components/MemeApp.js
+++ src/components/MemeApp.js
@@ -1,17 +1,17 @@
-import React, {useEffect,useState} from "react";
+import React from "react";
 import Header from "./Header";
 import MemeGenerator from "./MemeGenerator";
-import axios from "axios";
 
 function MemeApp() {
-    const [state, setState] = useState({
-        memes:[]
-    });
-    useEffect(() => {
-        axios.get("https://api.imgflip.com/get_memes")
-            // .then(response => setState({ memes: response.data}))
-            .then(response => console.log(response.data))
-    })
+    // const [state, setState] = useState({
+    //     memes:[]
+    // });
+
+    // useEffect(() => {
+    //     axios.get("https://api.imgflip.com/get_memes")
+    //         // .then(response => setState({ memes: response.data}))
+    //         .then(response => console.log(response.data))
+    // })
     return (
         <div className="container">
             <Header/>
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,24 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, {Component} from "react";
 
-function MemeGenerator() {
-    return (
-        <div className="meme-container">
-            <form>
-                <input type="text"
-                    placeholder="Input top text..."
-                />
-                <input type="text"
-                    placeholder="Input bottom text..."
-                />
-                <button>Generate</button>
-            </form>
-            <div className="meme">
-                <img alt="" src="https://i.imgflip.com/26am.jpg" />
-                <h2 className="top"></h2>
-                <h2 className="bottom"></h2>
+class MemeGenerator extends Component {
+    state = {
+        topText: "",
+        botText: "",
+        randomImage: "https://i.imgflip.com/26am.jpg",
+        allMemeImgs: []
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => this.setState({ allMemeImgs: response.data.memes }));
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
+        const randMemeImgUrl = this.state.allMemeImgs[randNum].url;
+        this.setState({randomImage: randMemeImgUrl})
+    }
+
+    render() {
+        return (
+            <div className="meme-container">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text"
+                        placeholder="Input top text..."
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        name="topText"
+                    />
+                    <input type="text"
+                        placeholder="Input bottom text..."
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        name="botText"
+                    />
+                    <button>Generate</button>
+                </form>
+                <div className="meme">
+                    <img alt="" src={this.state.randomImage} />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.botText}</h2>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+   
 }
 
 export default MemeGenerator
